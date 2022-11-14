f34ea65a9a4881bf3766e820b9e1bf87c60e151d
Mark Stowell <markstowell15@gmail.com>
2022-11-14 11:55:49 -0500

    restructured

---
+++ retrieve_prisoners.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include './php/database.php'; ?>
+    <?php 
+        $sql = 'SELECT * FROM prisoner';
+        $result = $conn->query($sql);
+        if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["prisoner_id"] . " - Name: " . $row["first_name"] . " " . $row["last_name"]. "<br>";
+            }
+        }
+        else echo "0 results";
+        $conn->close();
+    ?>
+</body>
+</html>
