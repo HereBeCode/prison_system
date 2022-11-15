dafb579d6cbaa2f2e202c03746f090de67c32131
Mark Stowell <markstowell15@gmail.com>
2022-11-14 20:21:22 -0500

    added comments

--- php/submit_visit.php
+++ php/submit_visit.php
@@ -6,6 +6,12 @@
     if(!($num_selected_protocols == 6)) die("All protocols not met.");
 
     $conf_number = $_POST['confirmationNumber'];
+
+    // Add logic for checking if visit date == current date ---> if not current date decline visit 
+    // Add logic for checking if patient has already been visited ---> if visited == 1, refuse visit.
+    // Any other stuff to check?
+    // Likely requires a SELECT query to grab the row with the visit_id == confirmationNumber, check for the logic above and reject the visit (i.e. do not execute the next two lines of code)
+
     $query = "UPDATE visits SET visited = 1 WHERE visit_id = $conf_number";
     $result = $conn->query($query);
    
