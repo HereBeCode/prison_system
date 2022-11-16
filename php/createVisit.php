c2761297b21e4fa78de4ac5f3a111ee7cdb44f2d
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2022-11-16 18:21:27 -0500

    finished 3 sql query maybe

--- php/createVisit.php
+++ php/createVisit.php
@@ -5,8 +5,10 @@
     $visitorID = $_POST['visitorID'];
     $requestedDate = $_POST['date'];
 
-    $lookupPastVisit = "SELECT visit_date from visits WHERE prisoner_id = $prisonerID AND visited = 1 ORDER BY visit_date DESC LIMIT 1" ;
+    $lookupPastVisit = "SELECT visit_date FROM visits WHERE prisoner_id = $prisonerID AND visited = 1 ORDER BY visit_date DESC LIMIT 1" ;
     
-    $lookup
+    $lookupSecurity = "SELECT security_level FROM prisoner WHERE prisoner_id = $prisonerID " ;
+
+    $lookupFutureVisit = "SELECT visit_date FROM visits WHERE prisoner_id = $prisonerID WHERE visit_date >= $requestedDate ORDER BY visit_date LIMIT 1";
 
 ?>
