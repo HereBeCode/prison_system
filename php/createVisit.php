50d5b363ef9cd2a85e0d3c1120b4baea36ebd46d
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2022-11-16 18:07:45 -0500

    Here is the first gross date query

--- php/createVisit.php
+++ php/createVisit.php
@@ -1,7 +1,11 @@
 <?php include 'database.php'; ?>
 <?php 
 
-    $sql = "SELECT * FROM prisoner";
+    $prisonerID = $_POST['prisonerID'];
+    $visitorID = $_POST['visitorID'];
+    $requestedDate = $_POST['date'];
+
+    $lookupPastVisit = "SELECT TOP 1 visit_date from visits WHERE prisoner_id = $prisonerID AND visited = 1 ORDER BY visit_date DESC" ;
     
 
 ?>
