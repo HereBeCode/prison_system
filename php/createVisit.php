cfc8422a897c50afc2a8768a6fb384acf9fd6ffc
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2022-11-18 12:07:27 -0500

    Testing date logic

--- php/createVisit.php
+++ php/createVisit.php
@@ -10,5 +10,20 @@
     $lookupSecurity = "SELECT security_level FROM prisoner WHERE prisoner_id = $prisonerID " ;
 
     $lookupFutureVisit = "SELECT visit_date FROM visits WHERE prisoner_id = $prisonerID WHERE visit_date >= $requestedDate ORDER BY visit_date LIMIT 1";
+    echo gettype($lookupPastVisit);
+    $datePastVisit = date_create($lookupPastVisit);
+    $dateFutureVisit = date_create($lookupFutureVisit);
+    $interval = date_diff($datePastVisit, $dateFutureVisit);
+    echo $interval->d;
 
+    if($lookupSecurity == "low"){
+
+    }
+    elseif($lookupSecurity == "medium"){
+
+    }
+    //else if for high and an else for failure or just leave it as else?
+    else{
+
+    }
 ?>
